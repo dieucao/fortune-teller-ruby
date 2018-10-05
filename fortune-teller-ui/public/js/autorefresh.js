a7565d41840c741b213f9017ccd507348cc6b299
Brian McClain <brianmmcclain@gmail.com>
2018-10-05 12:01:23 -0400

    Rewrote auto-refresh feature to be more consistant and not overwrite existing request parameters

--- fortune-teller-ui/public/js/autorefresh.js
+++ fortune-teller-ui/public/js/autorefresh.js
@@ -1,9 +1,36 @@
-function toggleAuto() {
+function checkAutorefresh() {
+    setInterval(autorefresh, 1000);
+}
+
+function autorefreshClicked() {
     var checkBox = document.getElementById("autorefresh");
 
-    if (checkBox.checked){
-        window.location = "/?auto=true";
-    } else {
-        window.location = "/";
+    // Remove the 'auto' param from the search path but
+    // keep all other params
+    if (!checkBox.checked) {
+        var reqURL = new URL(location.href);
+        var query_string = reqURL.search;
+        var params = new URLSearchParams(query_string);
+        params.delete('auto');
+        reqURL.search = params.toString();
+        var newURL = reqURL.toString();
+        window.location.href = newURL;
+    }
+}
+
+function autorefresh() {
+    var checkBox = document.getElementById("autorefresh");
+
+    // Rebuild the search path, setting the auto param but
+    // keeping all other params
+    var reqURL = new URL(location.href);
+    var query_string = reqURL.search;
+    var params = new URLSearchParams(query_string); 
+    params.set('auto', checkBox.checked);
+    reqURL.search = params.toString();
+    var newPath = reqURL.pathname + reqURL.search;
+
+    if (checkBox.checked) {
+        window.location.href = newPath;
     }
 }
